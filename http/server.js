985dacecf193dbfaeb3213ab3f911a41cfc957ba
myusername <lutt.ravenx@gmail.com>
2020-07-21 04:08:22 -0300

    Api include in home Page

--- http/server.js
+++ http/server.js
@@ -1,10 +1,11 @@
-  
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
 
 http.createServer((req, res) => {
+
+    
 
     const file = req.url === '/' ? 'index.html' : req.url
     const filePath = path.join(__dirname, 'public', file)
